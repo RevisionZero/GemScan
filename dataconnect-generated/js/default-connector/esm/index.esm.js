610e7b14980188b4de5dc3b1067e59c5325bb6f2
Ibrahim <itariq640@gmail.com>
2025-03-31 15:07:22 -0400

    Test push to full project

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'gemscan',
+  location: 'us-central1'
+};
+
--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  //apiKey: 'YOUR_API_KEY',
+  authDomain: 'GemScan.firebaseapp.com',
+  projectId: 'gemscan-dbb35',
+  storageBucket: 'GemScan.appspot.com',
+  messagingSenderId: '49242045596',
+  appId: '49242045596',
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
