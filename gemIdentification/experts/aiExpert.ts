cec2d13621704915b9df28b5e52f6c1bcd91a11e
Omar Shehada <awadomar24@gmail.com>
2025-04-06 16:42:51 -0400

    Working AI expert, adding AI expert to aggregator

--- gemIdentification/experts/aiExpert.ts
+++ gemIdentification/experts/aiExpert.ts
@@ -28,18 +28,46 @@ export default class AIExpert implements Expert{
         const options = {
             method: 'POST',
             headers: {Authorization: `Bearer ${apiKey.key}`, 'Content-Type': 'application/json'},
-            body: '{"model":"deepseek/deepseek-r1:free","messages":[{"role":"user","content":"What is the meaning of life?"}]}'
+            body: `{"model":"deepseek/deepseek-r1","messages":[{"role":"user","content":"Reply with one word: what is a gemstone with the following attributes: color:${gemstoneDescription.getColorString()}, luster:${gemstoneDescription.getShininess()}, transparency:${gemstoneDescription.getTransparency()}, size:${gemstoneDescription.getSize()} cm^3"}]}`
         };
 
         try {
             const response = await fetch(url, options);
             const data = await response.json();
-            console.log(data);
-            console.log('AI RESPONSE:\n',response)
+            const answer = data["choices"][0]["message"]["content"]
+            console.log('\n\n\n',response);
+            console.log('AI RESPONSE:\n',answer)
+            let result = new Result();
+            if(answer){
+                result.name = answer;
+                result.confidence = 0.5;
+                result.result = true;
+                result.gemstone = new Gemstone("",[0,0,0],0,"","","");
+                // resultGem.setColor([0,0,0]);
+                // resultGem.setShininess("");
+                // resultGem.setColorString("");
+                // resultGem.setSize(0);
+                // resultGem.setImage("");
+                // resultGem.setTransparency(gemstoneDescription.getTransparency());
+                return result;
+            }
+            else{
+                let emptyResult = new Result();
+                emptyResult.confidence = 0;
+                emptyResult.gemstone = new Gemstone();
+                emptyResult.result = false;
+                console.log('NO RESP')
+                return emptyResult;
+            }
         } catch (error) {
             console.error(error);
+            let emptyResult = new Result();
+            emptyResult.confidence = 0;
+            emptyResult.gemstone = new Gemstone();
+            emptyResult.result = false;
+            console.log('NO RESP')
+            return emptyResult;
         }
           
-        return new Result();
     };
 }
